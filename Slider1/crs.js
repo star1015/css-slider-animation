314403897d088ddf6d13bd06c367faeddd5fa2b7
starcoma <starcoma1015@gmail.com>
2019-11-28 14:39:21 +0800

    update slider 1

---
+++ Slider1/crs.js
@@ -0,0 +1,186 @@
+function AnimableArc(containerDiv, startAngle, width) {
+  var oChild = null;
+  var canvas, icon;
+  for (var i = 0; i < containerDiv.childNodes.length; i++) {
+    oChild = containerDiv.childNodes[i];
+    if (oChild.nodeName == "CANVAS") {
+      canvas = oChild;
+    } else if (oChild.nodeName == "DIV") {
+      this.iconContainer = oChild;
+      for (var j = 0; j < oChild.childNodes.length; j++) {
+        if (oChild.childNodes[j].nodeName == "IMG") {
+          this.icon = oChild.childNodes[j];
+        }
+      }
+    }
+  }
+  this.container = containerDiv;
+  this.color = canvas.getAttribute("data-fore-color");
+  this.back = canvas.getAttribute("data-back-color");
+  this.context = canvas.getContext("2d");
+  this.context.fillStyle = this.color;
+  this.startAngle = startAngle;
+  this.backEndAngle = Math.PI;
+  this.foreEndAngle = (canvas.getAttribute("data-end") / 100) * Math.PI * 2;
+  this.backCurrentAngle = this.startAngle;
+  this.foreCurrentAngle = this.startAngle;
+  this.backLoadingStep = this.backEndAngle / ((200 / 1000) * 60);
+  this.foreLoadingStep = this.foreEndAngle / ((300 / 1000) * 60);
+  this.foreEarsingStep = this.foreEndAngle / ((300 / 1000) * 60);
+  this.backEarsingStep = this.backEndAngle / ((300 / 1000) * 60);
+  this.stopTime = 2100;
+  this.onReSize(width);
+}
+AnimableArc.prototype.onReSize = function(foreSize) {
+  this.context.canvas.height = this.container.clientHeight;
+  this.context.canvas.width = this.container.clientWidth;
+  this.size = Math.min(this.container.clientWidth, this.container.clientHeight);
+  this.width = foreSize;
+  this.iconContainer.style.position = "absolute";
+  this.iconContainer.style.top = -foreSize + "px";
+  this.iconContainer.style.left = this.size / 2 - foreSize * 1.5 + "px";
+  this.iconContainer.style.width = foreSize * 3 + "px";
+  this.iconContainer.style.height = this.size / 2 + foreSize / 1 + "px";
+  this.iconContainer.style.transformOrigin = "center bottom";
+  this.icon.style.width = foreSize * 3 + "px";
+  this.icon.style.height = "auto";
+  this.icon.style.margin = "auto";
+  this.reDraw();
+};
+AnimableArc.prototype.rotateIcon = function(angle) {
+  this.iconContainer.style.webkitTransform = "rotate(" + angle + "rad)";
+  this.iconContainer.style.mozTransform = "rotate(" + angle + "rad)";
+  this.iconContainer.style.msTransform = "rotate(" + angle + "rad)";
+  this.iconContainer.style.oTransform = "rotate(" + angle + "rad)";
+  this.iconContainer.style.transform = "rotate(" + angle + "rad)";
+  this.icon.style.webkitTransform = "rotate(" + -angle + "rad)";
+  this.icon.style.mozTransform = "rotate(" + -(angle + this.foreLoadingStep) + "rad)";
+  this.icon.style.msTransform = "rotate(" + -(angle + this.foreLoadingStep) + "rad)";
+  this.icon.style.oTransform = "rotate(" + -(angle + this.foreLoadingStep) + "rad)";
+  this.icon.style.transform = "rotate(" + -(angle + this.foreLoadingStep) + "rad)";
+  if(this.foreCurrentAngle > this.startAngle + 0.1) {
+    this.iconContainer.style.display = "block";
+  } else {
+    this.iconContainer.style.display = "none";
+  }
+};
+AnimableArc.prototype.reDraw = function() {
+  var fore = new Path2D(),
+    back = new Path2D(),
+    center = this.size / 2,
+    context = this.context;
+
+  fore.arc(
+    center,
+    center,
+    (this.size - this.width) / 2,
+    this.startAngle,
+    this.foreCurrentAngle
+  );
+  back.arc(
+    center,
+    center,
+    (this.size - this.width) / 2,
+    this.startAngle,
+    this.backCurrentAngle
+  );
+  context.clearRect(0, 0, this.size, this.size);
+  context.lineWidth = this.width;
+  context.strokeStyle = "#ebebeb";
+  context.stroke(back);
+  context.strokeStyle = this.color;
+  context.stroke(fore);
+};
+AnimableArc.prototype.draw = function() {
+  if (
+    this.backCurrentAngle < this.backEndAngle + this.startAngle ||
+    this.foreCurrentAngle < this.foreEndAngle + this.startAngle
+  ) {
+    var fore = new Path2D(),
+      back = new Path2D(),
+      center = this.size / 2,
+      context = this.context;
+
+    var nextBackAngle = this.backCurrentAngle,
+      nextForeAngle = this.foreCurrentAngle;
+    if (this.backCurrentAngle < this.backEndAngle + this.startAngle) {
+      nextBackAngle += this.backLoadingStep;
+    } else {
+      nextForeAngle += this.foreLoadingStep;
+    }
+    fore.arc(
+      center,
+      center,
+      (this.size - this.width) / 2,
+      this.startAngle,
+      nextForeAngle
+    );
+    back.arc(
+      center,
+      center,
+      (this.size - this.width) / 2,
+      this.startAngle,
+      nextBackAngle
+    );
+    context.clearRect(0, 0, this.size, this.size);
+    context.lineWidth = this.width;
+    context.strokeStyle = "#ebebeb";
+    context.stroke(back);
+    context.strokeStyle = this.color;
+    context.stroke(fore);
+    this.backCurrentAngle = nextBackAngle;
+    this.foreCurrentAngle = nextForeAngle;
+    this.rotateIcon(this.foreCurrentAngle + Math.PI / 2 );
+    window.requestAnimationFrame(this.draw.bind(this));
+  } else {
+    const earse = this.earse.bind(this);
+    this.backCurrentAngle -= this.backLoadingStep;
+    this.foreCurrentAngle -= this.foreLoadingStep;
+    setTimeout(function() {
+      window.requestAnimationFrame(earse);
+    }, this.stopTime);
+  }
+};
+AnimableArc.prototype.earse = function() {
+  if (
+    this.backCurrentAngle > this.startAngle ||
+    this.foreCurrentAngle > this.startAngle
+  ) {
+    var fore = new Path2D(),
+      back = new Path2D(),
+      center = this.size / 2,
+      context = this.context;
+
+    var nextBackAngle = this.backCurrentAngle,
+      nextForeAngle = this.foreCurrentAngle;
+    if (this.foreCurrentAngle > this.startAngle) {
+      nextForeAngle -= this.foreEarsingStep;
+    } else {
+      nextBackAngle -= this.backEarsingStep;
+    }
+    fore.arc(
+      center,
+      center,
+      (this.size - this.width) / 2,
+      this.startAngle,
+      nextForeAngle
+    );
+    back.arc(
+      center,
+      center,
+      (this.size - this.width) / 2,
+      this.startAngle,
+      nextBackAngle
+    );
+    context.clearRect(0, 0, this.size, this.size);
+    context.lineWidth = this.width;
+    context.strokeStyle = "#ebebeb";
+    context.stroke(back);
+    context.strokeStyle = this.color;
+    context.stroke(fore);
+    this.rotateIcon(this.foreCurrentAngle + Math.PI / 2);
+    this.backCurrentAngle = nextBackAngle;
+    this.foreCurrentAngle = nextForeAngle;
+    window.requestAnimationFrame(this.earse.bind(this));
+  }
+};
--- Slider1/slider1.js
+++ Slider1/slider1.js
@@ -1,105 +1,59 @@
+var slide_1_red = null,
+  slide_1_yellow = null,
+  slide_1_green = null;
+var slide_1_red_container = null,
+  slide_1_yellow_container = null,
+  slide_1_green_container = null;
+
 function myFunction() {
-    var w = window.innerWidth;
-    var h = window.innerHeight;
-    $("#slider_1").roundSlider({
-        circleShape: "half-right",
-        sliderType: "min-range",
-        radius: w / 9,
-        showTooltip: false,
-        startAngle: 90,
-        handleSize: "+12",
-        width: w / 150
-    });
+  var w = window.innerWidth;
+  var h = window.innerHeight;
+  $("#slider_container").css("width", w * 73 / 300);
+  $("#slider_container").css("height", w * 73 / 300);
+  $("#slider_container").css("top", w / 19);
+  $("#slider_container").css("left", w / 13);
+  if (slide_1_red && slide_1_yellow && slide_1_green) {
+    slide_1_red.onReSize(w / 150);
+    slide_1_yellow.onReSize(w / 150);
+    slide_1_green.onReSize(w / 150);
+  }
+    document.getElementById("green_back").style.left = w / 4 + "px";
+    document.getElementById("yellow_back").style.left = w / 4 + "px";
+    document.getElementById("red_back").style.left = w / 4 + "px";
+    document.getElementById("green_back").style.top = 0 + "px";
+    document.getElementById("yellow_back").style.top = 0 + "px";
+    document.getElementById("red_back").style.top = 0 + "px";
+    document.getElementById("green_back").style.height = "auto";
+    document.getElementById("yellow_back").style.height = "auto";
+    document.getElementById("red_back").style.height = "auto";
 
-    $("#slider_1").css("top", w / 22);
-    $("#slider_2").roundSlider({
-        circleShape: "half-right",
-        sliderType: "min-range",
-        radius: w / 7.5,
-        showTooltip: false,
-        startAngle: 90,
-        handleSize: "+12",
-        width: w / 150
-    });
-    $("#slider_2").css("top", w / 42);
-    $("#slider_3").roundSlider({
-        circleShape: "half-right",
-        sliderType: "min-range",
-        showTooltip: false,
-        radius: w / 6.5,
-        startAngle: 90,
-        handleSize: "+12",
-        width: w / 150
-    });
-    $("#slider_3").css("top", "5px");
-    $("#slider_container").css("width", w / 4);
-    $("#slider_container").css("height", w / 4);
-    $("#slider_container").css("top", w / 24);
-    $("#slider_container").css("left", w / 35);
 }
-$(document).ready(function () {
-    myFunction();
-    init();
-    setTimeout(function () {
-        $("#slider_1").roundSlider({
-            value: -1
-        });
-
-        $("#slider_2").roundSlider({
-            value: -1
-        });
-        $("#slider_3").roundSlider({
-            value: -1
-        });
-
-    }, 2500);
-    setTimeout(function () {
-        $("#slider_1 .rs-handle.rs-move").css("display", "none");
-        $("#slider_2 .rs-handle.rs-move").css("display", "none");
-        $("#slider_3 .rs-handle.rs-move").css("display", "none");
-
-    }, 3000);
-
-
-})
+$(document).ready(function() {
+  myFunction();
+  init();
+});
 function init() {
-
-    $(".green").fadeIn(800).delay(1200).fadeOut(800);
-    $(".yellow").delay(500).fadeIn(800).delay(1400).fadeOut(800);
-    $(".red").delay(1000).fadeIn(800).delay(1600).fadeOut(800);
-
-    $("#slider_1").slideUp(1);
-    $("#slider_1").slideDown(300);
-    $("#slider_2").slideUp(1);
-    $("#slider_2").slideDown(300);
-    $("#slider_3").slideUp(1);
-    $("#slider_3").slideDown(300);
-    $("#slider_1 .rs-handle.rs-move").css("display", "none");
-    $("#slider_2 .rs-handle.rs-move").css("display", "none");
-    $("#slider_3 .rs-handle.rs-move").css("display", "none");
-
-    setTimeout(function () {
-        $("#slider_1 .rs-handle.rs-move").css("display", "block");
-        $("#slider_2 .rs-handle.rs-move").css("display", "block");
-        $("#slider_3 .rs-handle.rs-move").css("display", "block");
-
-        $("#slider_1").roundSlider({
-            value: 55
-        });
-
-        $("#slider_2").roundSlider({
-            value: 75
-        });
-        $("#slider_3").roundSlider({
-            value: 90
-        });
-
-    }, 500);
-    setTimeout(function () {
-        $("#slider_1").slideUp(800);
-        $("#slider_2").slideUp(800);
-        $("#slider_3").slideUp(800);
-
-    }, 3000);
-
+  $("#green_back").fadeIn(600).delay(1000).fadeOut(800);
+  $("#yellow_back").delay(300).fadeIn(800).delay(1200).fadeOut(800);
+  $("#red_back").delay(800).fadeIn(800).delay(1400).fadeOut(800);
+  var w = document.getElementById("slide_1_container").clientWidth;
+  var startingAngle = -(Math.PI / 2);
+  slide_1_red = new AnimableArc(
+    document.getElementById("slide_1_red"),
+    startingAngle,
+    w / 40
+  );
+  slide_1_yellow = new AnimableArc(
+    document.getElementById("slide_1_yellow"),
+    startingAngle,
+    w / 40
+  );
+  slide_1_green = new AnimableArc(
+    document.getElementById("slide_1_green"),
+    startingAngle,
+    w / 40
+  );
+  slide_1_red.draw();
+  slide_1_yellow.draw();
+  slide_1_green.draw();
 }
